02b071276f7e5f8a3433931f1e4a915a7ca7f8fe
totolog <totolog1126@gmail.com>
2020-08-22 22:59:30 +0900

    Finish user edit, update, index and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_035146) do
+ActiveRecord::Schema.define(version: 2020_08_22_134514) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
     t.string "remember_digest"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
