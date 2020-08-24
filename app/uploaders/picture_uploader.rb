2854eba2edc60c000e43c8d4df0944c0e08885cd
totolog <totolog1126@gmail.com>
2020-08-24 17:32:19 +0900

    finish

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,11 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
