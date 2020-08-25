8243e082fb309d54c6a87fdf15b5b8c82f9a6c26
totolog <totolog1126@gmail.com>
2020-08-25 19:28:13 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,15 @@ Rails.application.routes.draw do
   get "/login", to:"session#new"
   post "/login", to:"session#create"
   get "/logout", to:"session#destroy"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
-  resources :users
   resources :account_activations, only:[:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
   resources :microposts, only:[:create, :destroy]
+  resources :relationships, only:[:create, :destroy]
 end
 
